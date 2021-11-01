c2c58bba58f85fe8d83f5124c6132b646d3c7bfd
sylvester <abansekasly98@gmail.com>
2021-11-01 18:57:14 +0100

    Remove boilerPlate

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <div className ="header">
+			<nav>
+				<h1>navbar here</h1>
+			</nav>
+			<div className='text-content'>
+				<h2>work at the speed of though</h2>
+				<p>
+					 Most calendars are designed for teams. 
+					Slate is designed for freelancers who want
+					 a simple way to plan their schedule
+				</p>
+			</div>
+			<div className='screens'>
+				<h2>image here</h2>
+			</div>
+		</div>
+  );
+}
+
+export default Header;
+
