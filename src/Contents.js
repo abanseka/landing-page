47fddbf9f6859d4822b559ae1e0593c24c681e1d
sylvester <abansekasly98@gmail.com>
2021-11-03 10:08:40 +0100

    Contents-section-styling : Done

--- src/Contents.js
+++ src/Contents.js
@@ -9,31 +9,35 @@ const Contents = () => {
 			<div className = 'text-content'>
 				<h1>Contents</h1>
 				<p>
-					We focus on ergonomics and meeting you where you work. 
+					We focus on ergonomics and meeting you where you work.<br/>
 					It's only a keystroke away.
 				</p>
 			</div>
 
 			<div className="cards">
-				<div className='work-card'>
-					<h4>Work</h4>
-					<p>
-					Ever wondered if you're too reliant 
-					on a client for work? Slate helps 
-					you identify .
-					</p>
-					<button >Sign Up</button><br/>
+				<div className='work-card card'>
+					<div className='card-heading'>
+						<h4>Work</h4>
+						<p>
+							Ever wondered if you're too reliant
+							on a client for work? Slate helps 
+							you identify .
+						</p>
+					</div>
+					<button >Sign Up</button>
 					<img   src = {contentImage1} alt='Screen Mask'/>
 				</div>
 
-				<div className='design-card'>
-				<h4>Design with real data</h4>
-					<p>
-					Ever wondered if you're too reliant 
-					on a client for work? Slate helps 
-					you identify .
-					</p>
-					<button >Try For Free</button><br/>
+				<div className='design-card card'>
+					<div className='card-heading'>
+						<h4>Design with real data</h4>
+						<p>
+						Ever wondered if you're too reliant 
+						on a client for work? Slate helps 
+						you identify .
+						</p>
+					</div>
+					<button >Try For Free</button>
 					<img   src = {contentImage2} alt='Boards Notifications'/>
 				</div>
 
